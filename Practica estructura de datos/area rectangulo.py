9038a241e779744ed1da53aecc74e06ed568bf56
Ruddy.Almonte <ran.djc2@gmail.com>
2025-04-05 10:16:47 -0400

    primer ejercicio

---
+++ Practica estructura de datos/area rectangulo.py
@@ -0,0 +1,6 @@
+# 1 Calcular el área de un rectángulo
+
+largo = float(input("Ingresa el largo del rectángulo: "))
+ancho = float(input("Ingresa el ancho del rectángulo: "))
+area = largo * ancho
+print(f"El área del rectángulo es: {area}")
