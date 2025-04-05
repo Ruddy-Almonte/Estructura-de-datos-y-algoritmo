447e27dc948831a0efb20cfd72cd855b1e51c74b
Ruddy.Almonte <ran.djc2@gmail.com>
2025-04-05 10:44:43 -0400

    ejercicio6

---
+++ Practica estructura de datos/operacion con numeros.py
@@ -0,0 +1,12 @@
+
+# 6 Suma, resta, multiplicación y división
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+print(f"Suma: {num1 + num2}")
+print(f"Resta: {num1 - num2}")
+print(f"Multiplicación: {num1 * num2}")
+if num2 != 0:
+    print(f"División: {num1 / num2}")
+else:
+    print("No se puede dividir entre cero.")
