bc2886f139002e94051e12a56390e9d4938c1ac9
Ruddy.Almonte <ran.djc2@gmail.com>
2025-04-12 18:56:35 -0400

    2 - diccionario

--- Practica estructura de datos/Practica2 estructura de datos/tupla con 5 países.py
+++
@@ -1,4 +0,0 @@
-
-paises = ("México", "Colombia", "Argentina", "España", "Chile")
-print("El tercer país es:", paises[2])  
-
---
+++ Practica2 estructura de datos/2diccionario.py
@@ -0,0 +1,7 @@
+
+persona = {
+    "nombre": "Maria",
+    "edad": 26,
+    "ciudad": "Santiago"
+}
+print("La edad es:", persona["edad"])
